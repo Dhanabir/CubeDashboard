33ff196915eae30825febf1331459d53f92974df
root <Dhanabirchakma1@gmail.com>
2023-12-17 21:23:08 +0530

    Barchart modification changes 1.1

--- dashboard-frontend/src/App.js
+++ dashboard-frontend/src/App.js
@@ -192,39 +192,8 @@ class App extends Component {
             />
           </Col>
         </Row>
+        <br />
         <Row>
-          <Col sm="6">
-            <Chart
-              cubejsApi={cubejsApi}
-              title="New Users Over Time"
-              query={{
-                measures: ["Users.count"],
-                timeDimensions: [
-                  {
-                    dimension: "Users.createdAt",
-                    dateRange: ["2017-01-01", "2018-12-31"],
-                    granularity: "month",
-                  },
-                ],
-              }}
-              render={(resultSet) => (
-                <ResponsiveContainer width="100%" height={300}>
-                  <AreaChart data={resultSet.chartPivot()}>
-                    <XAxis dataKey="category" tickFormatter={dateFormatter} />
-                    <YAxis tickFormatter={numberFormatter} />
-                    <Tooltip labelFormatter={dateFormatter} />
-                    <Area
-                      type="monotone"
-                      dataKey="Users.count"
-                      name="Users"
-                      stroke="rgb(106, 110, 229)"
-                      fill="rgba(106, 110, 229, .16)"
-                    />
-                  </AreaChart>
-                </ResponsiveContainer>
-              )}
-            />
-          </Col>
           <Col sm="6">
             <Chart
               cubejsApi={cubejsApi}
@@ -270,6 +239,38 @@ class App extends Component {
                   </ResponsiveContainer>
                 );
               }}
+            />
+          </Col>
+          <Col sm="6">
+            <Chart
+              cubejsApi={cubejsApi}
+              title="New Users Over Time"
+              query={{
+                measures: ["Users.count"],
+                timeDimensions: [
+                  {
+                    dimension: "Users.createdAt",
+                    dateRange: ["2017-01-01", "2018-12-31"],
+                    granularity: "month",
+                  },
+                ],
+              }}
+              render={(resultSet) => (
+                <ResponsiveContainer width="100%" height={300}>
+                  <AreaChart data={resultSet.chartPivot()}>
+                    <XAxis dataKey="category" tickFormatter={dateFormatter} />
+                    <YAxis tickFormatter={numberFormatter} />
+                    <Tooltip labelFormatter={dateFormatter} />
+                    <Area
+                      type="monotone"
+                      dataKey="Users.count"
+                      name="Users"
+                      stroke="rgb(106, 110, 229)"
+                      fill="rgba(106, 110, 229, .16)"
+                    />
+                  </AreaChart>
+                </ResponsiveContainer>
+              )}
             />
           </Col>
         </Row>
